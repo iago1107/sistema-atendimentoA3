005d3b2b8a3326071f734cc7b2dc2b5d40295e28
iago1107 <iago1107@hotmail.com>
2025-11-06 22:06:57 -0300

    Programa e banco de dados fucionando

--- src/main/java/SistemaDePedidosApplication/SistemaAtendimentoApplication.java
+++ src/main/java/SistemaDePedidosApplication/SistemaAtendimentoApplication.java
@@ -1,10 +1,13 @@
 package SistemaDePedidosApplication;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaAtendimentoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaAtendimentoApplication.class, args);
+		System.out.println("✅ Sistema iniciado — conectado ao banco de dados com sucesso!");
 	}
 }
